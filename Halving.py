bfa12fdbbb2c838ed3abc9ae497952671ebab4d1
Mikko Setälä <setala3@hotmail.com>
2018-12-29 20:31:38 +0200

    Update Halving.py
    Some explanation and cleaning made.

--- Halving.py
+++ Halving.py
@@ -1,14 +1,26 @@
 import random
 
-highest = 1000000
+highest = 1000000 # maximum value of random number.
 
-times = set() 
+"""
+We set here some variables:
+- right answer we try to guess.
+- three variables related to guessing process, including the current guess.
+- all guesses made so far.
+"""
 
 right_answer = random.randint(1, highest)
 guess_answer = -1
 guess_high = highest
 guess_low = 0 
 answers = []
+
+"""
+Algorithm:
+- set guess in the middle of low to high guess range.
+- depending the comparison to right answer (higher or lower) we adjust boundaries.
+- while loop goes to last else on match and tells the amount of guesses used.
+"""
 
 while guess_answer != right_answer:
    guess_answer = guess_low + int((guess_high - guess_low)/2) 
